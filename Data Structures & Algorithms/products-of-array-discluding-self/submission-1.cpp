ee228af4b2680c71f64a4e9d5a0adcebb0bd7495
Nischay Goswami <nischaygoswami75@gmail.com>
2026-08-31 02:13:03 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int pro{1},z{};
+        
+        for(const int& x:nums){
+            if(!x)z++;
+            else pro*=x;
+        }
+        if(!z)for(int i{};i<nums.size();i++)nums[i]=pro/nums[i];
+        else if(z==1)for(int i{};i<nums.size();i++){
+            if(!nums[i])nums[i]=pro;
+            else nums[i]=0;
+            }
+        else for(int i{};i<nums.size();i++)nums[i]=0;
+        return nums;
+    }
+};
